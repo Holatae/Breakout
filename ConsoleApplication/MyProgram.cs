f22e82e652119cd77e7f48651ee2963815ca99f4
Victor Andersson <victor.andersson2@elev.ga.ntig.se>
2021-10-07 14:31:29 +0200

    Refactored code

--- ConsoleApplication/MyProgram.cs
+++ ConsoleApplication/MyProgram.cs
@@ -5,6 +5,9 @@ public class MyProgram
     private int trackLength = 17;
     private int x = 1;
     private int y = 1;
+
+    int stageLengthX = 15;
+    int stageLengthY = 19;
     public void Run()
     {
         Forward();
@@ -43,6 +46,44 @@ public class MyProgram
             Console.Write(" ");
         }
         Forward();
+    }
+
+    public void Draw()
+    {
+        
+        y = 0;
+        x = stageLengthX;
+        //Right wall
+        Console.SetCursorPosition(x, y);
+        for (int i = 0; i < stageLengthY; i++)
+        {
+            Console.WriteLine("@");
+            y += 1;
+            Console.SetCursorPosition(x, y);
+
+        }
+
+        y = 19;
+        x = 1;
+        //Floor
+        Console.SetCursorPosition(x, y);
+        for (int i = 0; i < stageLengthX - 1; i++)
+        {
+            Console.WriteLine("-");
+            x += 1;
+            Console.SetCursorPosition(x, y);
+        }
+
+        y = 0;
+        x = 1;
+        //Roof
+        Console.SetCursorPosition(x, y);
+        for (int i = 0; i < stageLengthX - 1; i++)
+        {
+            Console.WriteLine("-");
+            x += 1;
+            Console.SetCursorPosition(x, y);
+        }
     }
 
 }
