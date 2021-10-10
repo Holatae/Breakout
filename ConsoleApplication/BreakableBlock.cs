5428bbf65e84e7ed420e03e4ee0a3f26014e8112
Victor Andersson <vicand.05.va@gmail.com>
2021-10-10 16:45:16 +0200

    Started on implementation of brakeable block

--- ConsoleApplication/BreakableBlock.cs
+++ ConsoleApplication/BreakableBlock.cs
@@ -6,6 +6,7 @@ namespace ConsoleApplication
     {
         private int posX;
         private int posY;
+        private int row;
         private bool isBrocken = false;
 
         void BreakBlock()
@@ -14,5 +15,13 @@ namespace ConsoleApplication
             Console.Write(" ");
         }
 
+        public BreakableBlock(int posX, int posY, int row)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.row = row;
+            Console.SetCursorPosition(this.posX, this.posY);
+            Console.Write($"{ConsoleColor.Blue}@");
+        }
     }
 }
