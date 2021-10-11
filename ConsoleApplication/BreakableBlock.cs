60bee10e9c686093b08f03d25c9d98ed97d74500
Victor Andersson <=>
2021-10-11 15:22:31 +0200

    Blocks works

--- ConsoleApplication/BreakableBlock.cs
+++ ConsoleApplication/BreakableBlock.cs
@@ -4,8 +4,8 @@ namespace ConsoleApplication
 {
     public class BreakableBlock
     {
-        public int PosX { get; }
-        public int PosY { get; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
         private int row;
         private bool isBrocken = false;
 
@@ -21,7 +21,15 @@ namespace ConsoleApplication
             PosY = posY;
             this.row = row;
             Console.SetCursorPosition(this.PosX + 1, this.PosY);
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("$");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void Destroy()
+        {
+            PosX = Int32.MaxValue;
+            PosY = Int32.MaxValue;
         }
     }
 }
